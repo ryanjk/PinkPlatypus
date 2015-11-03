3abdccecf199f6d7031c98189db69861053eb268
ryanjk <ryankitner@gmail.com>
2015-11-03 01:09:40 -0500

    Add ItemInventory and ItemDatabase script

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public static bool isValidItem(int itemID) {
+        return true;
+    }
+
+    public static string getName(int itemID) {
+        return "";
+    }
+
+    public static bool isKeyItem(int itemID) {
+        return true;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+}
---
+++ Assets/Scripts/ItemInventory.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+/**
+* ItemInventory
+* @author Ryan Kitner
+* Add this component to a GameObject to give it an inventory of items. 
+* 
+* Notable design decisions:
+* 
+* - Adding an item cannot fail (i.e. doesn't return a boolean). The responsibility of ensuring something like a key item is only
+*   added once should fall upon the caller. It would be wasteful making checks everytime an item is added, and I'm also trying 
+*   to minimize the responsibility of this class.
+*
+* - An amount of an item will only be removed if it has enough to remove exactly the quantity requested. So if the inventory
+*   contains 50 gold, only calls removing 50 gold or less will return true. 
+*
+* Finally, I used assertions for last-second error detection. Assertions are only compiled into the development build, and the
+* idea with them is to catch programmers contradicting any assumptions the API makes. For example, all the methods assert that 
+* the item ID passed to it corresponds to an item in the global database. If this isn't the case, the assertion will fail and
+* if Assert.raiseExceptions is set to true, the execution will stop. Otherwise it will print a message and continue.
+*/
+public class ItemInventory : MonoBehaviour {
+
+    /**
+    * Add an item to the inventory
+    * @param itemID: ID of the item to add
+    */
+    public void addItem(int itemID) {
+        addItem(itemID, 1);
+    }
+
+    /**
+    * Add an item to the inventory
+    * @param itemID: ID of the item to add
+    * @param quantity: Number of items to add
+    */
+    public void addItem(int itemID, int quantity) {
+        Assert.IsTrue(ItemDatabase.isValidItem(itemID), "ItemInventory: Cannot add item. Not valid ID.");
+        Assert.IsTrue(quantity >= 0, "ItemInventory: Cannot add item. Must add quantity >= 0");
+
+        if (_items.ContainsKey(itemID)) {
+            _items[itemID] += quantity;
+        }
+        else {
+            _items.Add(itemID, quantity);
+        }
+    }
+
+    /**
+    * Remove an item from the inventory
+    * @param itemID: ID of the item to add
+    * @return bool true if item is successfully removed, false otherwise
+    */
+    public bool removeItem(int itemID) {
+        return removeItem(itemID, 1);
+    }
+
+    /**
+    * Remove an item from the inventory
+    * @param itemID: ID of the item to add
+    * @param quantity: Number of items to remove
+    * @return bool true if the exact quantity of items are successfully removed, false otherwise
+    */
+    public bool removeItem(int itemID, int quantity) {
+        Assert.IsTrue(ItemDatabase.isValidItem(itemID), "ItemInventory: Cannot remove item. Not valid ID.");
+        Assert.IsTrue(quantity >= 0, "ItemInventory: Cannot remove item. Must remove quantity >= 0");
+
+        int current_count = countItem(itemID);
+        if (quantity > current_count) {
+            return false;
+        }
+        else {
+            _items[itemID] -= quantity;
+            return true;
+        }
+    }
+
+    /**
+    * Count the amount of a given item in the inventory
+    * @param itemID: ID of the item to count
+    * @return int the total amount of the item in the inventory 
+    */
+    public int countItem(int itemID) {
+        Assert.IsTrue(ItemDatabase.isValidItem(itemID), "ItemInventory: Cannot count item. Not valid ID.");
+
+        if (!_items.ContainsKey(itemID)) {
+            return 0;
+        }
+
+        else {
+            return _items[itemID];
+        }
+
+    }
+
+    void Start () {
+        _items = new Dictionary<int, int>();
+	}
+
+    private Dictionary<int, int> _items;
+}
