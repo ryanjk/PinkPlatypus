c4dada11f253581c679c16993c2ff77490801288
Harry Schneidman <hschneidman@gmail.com>
2015-11-03 22:00:05 -0500

    First commit from Harry; Created TemplateDungeon scene and imported standard assets
    Also made a flashlight object and script and some materials.
    
    This was mostly done to see how the project transfers between machines.

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+	public Light flashlight;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.F))
+			flashlight.enabled = !flashlight.enabled;
+	}
+}
