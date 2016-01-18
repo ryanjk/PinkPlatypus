33dabab0ef0b9a52ba42e13576fe3bb0782e1e31
Alex D <ellxandra@yahoo.ca>
2016-01-18 16:26:14 -0500

    Added portal room scene
    3D Portal Room scene. All the portals redirect to the Template Room.

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -12,7 +12,7 @@ public class PlayerMain : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         //TODO: fix this, can't create a script with new. should be AddComponent.
 		//_inventory = new ItemInventory ();
-        _sceneLoadData = new SceneLoadData();
+       // _sceneLoadData = new SceneLoadData();
 
 	}
     void FixedUpdate()
@@ -22,13 +22,47 @@ public class PlayerMain : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+        //Updated upstream
         rb.AddForce(movement * speed);
     }
+
+	/**
+    * Add an item to the inventory
+    * @param itemID: ID of the item to add
+    * @param quantity: Number of items to add
+    */
+	public void addItem(int pId, int pQuantity){
+		_inventory.addItem (pId, pQuantity);
+	}
+
+	/**
+    * Remove an item from the inventory
+    * @param itemID: ID of the item to add
+    * @return bool true if item is successfully removed, false otherwise
+    */
+	public bool removeItem(int pItemID) {
+		return _inventory.removeItem(pItemID, 1);
+	}
+
+	/**
+    * Remove an item from the inventory
+    * @param itemID: ID of the item to add
+    * @param quantity: Number of items to remove
+    * @return bool true if the exact quantity of items are successfully removed, false otherwise
+    */
+	public bool removeItem(int pItemID, int pQuantity) {
+		return _inventory.removeItem(pItemID, pQuantity);
+	}
+	/*
+	public Vector3 getPosition(){
+
+	}*/
+
 
 	private ItemInventory _inventory;
 
     //TODO: change to private, add access methods
-    public SceneLoadData _sceneLoadData;
+   // public SceneLoadData _sceneLoadData;
 
 } 
 	
