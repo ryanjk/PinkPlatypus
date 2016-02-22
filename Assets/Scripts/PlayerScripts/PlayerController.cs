871bcb58bc7d560499a7dbda755dacfa6abc4dff
ryanjk <ryankitner@gmail.com>
2016-02-22 14:09:01 -0500

    PlayerController snapping

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour {
     private float maxSpeed = 4f;
     private float minSpeed = 0.1f;
     private int direction = 0;
+
+    private Direction prev_direction = Direction.NONE;
+    private float speed = 4.0f;
     private Rigidbody _rbody;
 
 	void Start () {
@@ -32,37 +35,80 @@ public class PlayerController : MonoBehaviour {
         float vert = Input.GetAxis("Vertical");
         float hori = Input.GetAxis("Horizontal");
 
-        if ((direction == 0 && vert > minSpeed) || (direction == 2 && vert < -minSpeed)) {
-            _rbody.velocity = new Vector3(0f, 0f, maxSpeed * vert);
-        } else if ((direction == 1 && hori < -minSpeed) || (direction == 3 && hori > minSpeed)) {
-            _rbody.velocity = new Vector3(maxSpeed * hori, 0f, 0f);
-        } else {
-            if (vert > minSpeed)
-            {
-                changeDirection(0);
-                _rbody.velocity = new Vector3(0f, 0f, maxSpeed * vert);
-            }
-            else if (hori > minSpeed)
-            {
-                changeDirection(3);
-                _rbody.velocity = new Vector3(maxSpeed * hori, 0f, 0f);
-            }
-            else if (vert < -minSpeed)
-            {
-                changeDirection(2);
-                _rbody.velocity = new Vector3(0f, 0f, maxSpeed * vert);
-            }
-            else if (hori < -minSpeed)
-            {
-                changeDirection(1);
-                _rbody.velocity = new Vector3(maxSpeed * hori, 0f, 0f);
-            }
-            else _rbody.velocity = new Vector3(0f, 0f, 0f);
-        }
-	}
+        var current_direction = input_to_direction(vert, hori);
 
-   private void changeDirection (int pDirection) {
-        direction = pDirection;
-        transform.GetChild(0).transform.localEulerAngles = new Vector3(90f, 0f, pDirection * 90f);
-   }
+        // check if changing direction
+        if (current_direction == prev_direction) {
+            // not changing, keep going
+            _rbody.velocity = direction_to_velocity(current_direction);
+            set_sprite(current_direction);
+        }
+
+        else if (current_direction != prev_direction) {
+            // changing, make sure player is in the center of a tile before changing
+            var on_x = _rbody.position.x % 0.5f;
+            var on_z = _rbody.position.z % 0.5f;
+            var epsilon = 0.05f;
+            if (( on_x < epsilon ) && (on_z < epsilon)) {
+                // player is in the center, can change
+                _rbody.MovePosition(new Vector3(_rbody.position.x - on_x, _rbody.position.y, _rbody.position.z - on_z));
+                _rbody.velocity = direction_to_velocity(current_direction);
+                set_sprite(current_direction);
+                prev_direction = current_direction;
+            }
+        }
+
+    }
+
+    private void set_sprite (Direction direction) {
+        if (direction == Direction.NONE) return;
+
+        transform.GetChild(0).transform.localEulerAngles = new Vector3(90f, 0f, (int) direction * 90f);
+    }
+
+    private Direction input_to_direction(float vertical, float horizontal) {
+        if (vertical < 0.0f) {
+            return Direction.DOWN;
+        }
+
+        else if (vertical > 0.0f) {
+            return Direction.UP;
+        }
+
+        else if (horizontal < 0.0f) {
+            return Direction.LEFT;
+        }
+
+        else if (horizontal > 0.0f) {
+            return Direction.RIGHT;
+        }
+        else {
+            return Direction.NONE;
+        }
+    }
+
+    private Vector3 direction_to_velocity(Direction dir) {
+        switch (dir) {
+            case Direction.UP:
+                return new Vector3(0.0f, 0.0f, speed);
+            case Direction.DOWN:
+                return new Vector3(0.0f, 0.0f, -speed);
+            case Direction.LEFT:
+                return new Vector3(-speed, 0.0f, 0.0f);                
+            case Direction.RIGHT:
+                return new Vector3(speed, 0.0f, 0.0f);
+            case Direction.NONE:
+                return new Vector3(0.0f, 0.0f, 0.0f);
+            default:
+                return new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    private enum Direction {
+        UP,
+        LEFT,
+        DOWN,
+        RIGHT,
+        NONE
+    }
 }
