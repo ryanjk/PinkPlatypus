b64a1251150a8baccc03a8355247c26200023afb
Alex D <ellxandra@yahoo.ca>
2016-02-26 11:24:55 -0500

    Menu skeleton

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public bool newGame;
+	public bool saveGame;
+	public bool loadGame;
+	public bool scores;
+	public bool addPlayer;
+	public bool viewItems;
+	public bool exit;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseUp(){
+		if (newGame) {
+			Application.LoadLevel(1);
+		}
+		if (loadGame) {
+		}
+		if (scores) {
+		}
+		if (exit) {
+			Application.Quit();
+		}
+	}
+}
