baa092160f5a8cb842f975fd8323457b9c5ccb92
ellxandra <ellxandra@yahoo.ca>
2016-03-12 19:00:00 -0500

    Pause Menu transition
    Added pause menu transition for all scenes

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuScript : MonoBehaviour {
+
+	private int _level;
+	private bool _paused;
+	// Use this for initialization
+	void Start () {
+		_level = Application.loadedLevel;
+		_paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			if (_paused.Equals (true)) {
+				
+				foreach (Transform child in this.transform){
+					child.gameObject.SetActive (true);
+				}
+				Application.LoadLevel (_level);
+				_paused = false;
+			}
+			else{
+				_level = Application.loadedLevel;
+				Application.LoadLevel (5);
+				//gameObject.SetActive (false);
+
+				foreach (Transform child in this.transform){
+				child.gameObject.SetActive(false);
+				}
+
+
+				_paused = true;
+			}
+
+		}
+	}
+}
