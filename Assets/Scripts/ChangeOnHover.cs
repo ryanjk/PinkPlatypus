6b5750e9e95d19f1a50c6595186aa2001f0d01cb
hschneidman <hschneidman@gmail.com>
2016-04-10 12:59:55 -0400

    Update from master (attempt 1)

---
+++ Assets/Scripts/ChangeOnHover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeOnHover : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color=Color.cyan;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+	void OnMouseEnter(){
+		GetComponent<Renderer>().material.color=Color.green;
+	}
+	void OnMouseExit(){
+		GetComponent<Renderer>().material.color=Color.cyan;
+    }
+} 
